f9cfcfdbca53f8d6161bc1372dbbec07a1f8641b
iareebahmad <m.areeb.ahmad@gmail.com>
2025-06-13 20:16:17 +0530

    Index UI

--- taskApp.py
+++ taskApp.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import webbrowser
 
 st.title("Dual Database Task Logger")
 st.header("Please choose your preferred Database")
 
-st.write("This is a simple Streamlit app.")
+db = st.selectbox("Choose your Database : ",["Select an Option","SQLite3","PostgreSQL"])
+urls = {"SQLite3": "https://github.com","PostgreSQL": "https://youtube.com"}
+
+# Button to trigger redirection
+if st.button("Use this Database"):
+    if db != "Select an option":
+        st.success(f"Opening {db} Database Task Logger")
+        webbrowser.open_new_tab(urls[db])
+    else:
+        st.warning("Please select a website first.")
